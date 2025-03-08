33f7b4cd35334930b99363067235022026e8e14b
Aman123-cmd <amanjavc@gmail.com>
2025-03-08 12:04:53 +0530

    Updated backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,34 +1,44 @@
 import express from "express";
-import AuthRoutes from "./routes/auth.route.js";
-import MessageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import bodyParser from "body-parser";
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5001;
 
-// Middleware
-app.use(express.json({ limit: "50mb" })); // Parse JSON payloads with a size limit of 50MB
-app.use(express.urlencoded({ limit: "50mb", extended: true })); // Parse URL-encoded data with a size limit of 50MB
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "50mb" })); 
+app.use(express.urlencoded({ limit: "50mb", extended: true })); 
 app.use(cookieParser());
-
-// CORS Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow only this origin
-    credentials: true, // Allow cookies and authorization headers
+    origin: "http://localhost:5173",
+    credentials: true,
   })
 );
+app.use(express.json({ limit: "50mb" })); // Parse JSON payloads with a size limit of 50MB
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // Parse URL-encoded data with a size limit of 50MB
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-// Routes
-app.use("/api/auth", AuthRoutes);
-app.use("/api/messages", MessageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// Start Server
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT, () => {
-  console.log(`Server is started on port ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
