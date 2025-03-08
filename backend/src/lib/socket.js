e3b1003058c68124674f177c17aa6e8f353aa133
Aman123-cmd <amanjavc@gmail.com>
2025-03-08 09:54:07 +0530

    Updated backend

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -33,5 +33,23 @@ io.on("connection", (socket) => {
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.id);
+  
+  socket.on("offer", (data) => {
+    socket.to(data.target).emit("offer", data);
+  });
 
+  socket.on("answer", (data) => {
+    socket.to(data.target).emit("answer", data);
+  });
+
+  socket.on("candidate", (data) => {
+    socket.to(data.target).emit("candidate", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected: ", socket.id);
+  });
+});
 export { io, app, server };
